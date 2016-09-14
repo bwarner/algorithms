43cf0c3eba6ec184ca426957aeae252f5863d5cd
Byron Warner <bfwarner@gmail.com>
2016-09-13 21:17:08 -0700

    Initial commit

---
+++ tests/ReverseSpec.js
@@ -0,0 +1,9 @@
+var Node = require ("../lib/list_node");
+
+
+describe('Test Lists Operations', function() {
+  it('Can reverse a list recursively', function() {
+    var list = Node(1, Node(2, Node(3, Node(4))));
+
+  });
+});
