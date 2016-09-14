716ac885ee0d4ea4ba4f4505ffaf1458d3e3d36f
Byron Warner <bfwarner@gmail.com>
2016-09-13 23:28:49 -0700

    Working Linked List Reverse

--- tests/ReverseSpec.js
+++ tests/ReverseSpec.js
@@ -1,9 +1,35 @@
-var Node = require ("../lib/list_node");
-
+var LinkedList = require ("../lib/list_node");
+var assert = require('chai').assert;
 
 describe('Test Lists Operations', function() {
-  it('Can reverse a list recursively', function() {
-    var list = Node(1, Node(2, Node(3, Node(4))));
+  describe("Recursively", function(){
+    it('Can reverse a list recursively', function() {
+      var list = new LinkedList(1, 2, 3, 4);
+      var expected = list.toArray().reverse();
+      var list = list.reverse();
+      assert.deepEqual(expected, list.toArray());
+    });
 
+    it('Can an empty list ', function() {
+      var list = new LinkedList();
+      var values = list.reverse().toArray();
+      assert.lengthOf(values, 0);
+    });
   });
+
+  describe("Iteratively", function(){
+    it('Can reverse a list recursively', function() {
+      var list = new LinkedList(1, 2, 3, 4);
+      var expected = list.toArray().reverse();
+      list.iterativeReverse();
+      assert.deepEqual(expected, list.toArray());
+    });
+
+    it('Can an empty list ', function() {
+      var list = new LinkedList();
+      list.iterativeReverse(null);
+      assert.lengthOf(list.toArray(), 0);
+    });
+  });
+
 });
